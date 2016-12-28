041fb9354705d0c291a6a90960464b67cb7c5280
Dave Perlman <daveperlman89@me.com>
2016-12-28 13:13:37 -0500

    Most recent design update - basic responsive design as well as updated color scheme

--- index.php
+++ index.php
@@ -7,11 +7,12 @@ if( isset($_POST['upload']) ) {
 	$unserialized = unserialize($_SESSION['user']);
 	$unserialized->image_upload($_FILES['to-upload'], $_POST['caption']);
 }
-$images = $pdo->query("SELECT * FROM images")->fetchAll(PDO::FETCH_ASSOC);
+$images = $pdo->query("SELECT * FROM images LIMIT 20")->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="_css/style.css">
 		<link rel="stylesheet" href="">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
@@ -20,7 +21,7 @@ $images = $pdo->query("SELECT * FROM images")->fetchAll(PDO::FETCH_ASSOC);
 	<body>
 		<header>
 			<nav>
-			<h1>Gallery</h1>
+			<h1>Responsive Image Gallery</h1>
 				<ul class="nav-ul">
 					<li><a href="index.php"><span>Home</span></a></li>
 					<li><a href="admin.php"><span>Admin</span></a></li>
@@ -46,7 +47,7 @@ $images = $pdo->query("SELECT * FROM images")->fetchAll(PDO::FETCH_ASSOC);
 					<div id="image-wrapper">
 						<?php foreach ($images as $img): ?>
 								<div class="img-panel">
-									<img class="thumbnail"src="<?php echo $img[path]; ?>" alt="">
+									<img class="thumbnail"src="<?php echo $img[path]; ?>" data-image='<?php echo $img[path]; ?>'>
 									<p class="caption"><?php echo $img['caption']; ?></p>
 								</div>
 						<?php endforeach ?>
@@ -63,16 +64,5 @@ $images = $pdo->query("SELECT * FROM images")->fetchAll(PDO::FETCH_ASSOC);
 				<p>&#169 Gallery Example 1989 - 2017</p>
 			</footer>
 		</div>
-		<script>
-			$('.img-panel').click( function() {
-				$(this).addClass('modal');
-				$(this).removeClass('img-panel');
-			});
-
-			$('.modal').click( function() {
-				$(this).addClass('img-panel');
-				$(this).removeClass('modal');
-			});
-		</script>
 	</body>	
 </html>
